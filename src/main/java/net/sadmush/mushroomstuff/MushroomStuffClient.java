1c4bf861619d8b6adfa1455e01de573022126ee4
Stars-Universeal-Coding-Space <96582453+Stars-Universeal-Coding-Space@users.noreply.github.com>
2024-11-25 19:34:51 +1100

    Initial commit

---
+++ src/main/java/net/sadmush/mushroomstuff/MushroomStuffClient.java
@@ -0,0 +1,10 @@
+package net.sadmush.mushroomstuff;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MushroomStuffClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
