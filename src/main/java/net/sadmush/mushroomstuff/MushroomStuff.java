b9e56ccca8268cfdd65d21023017beec2b905cf5
Stars-Universeal-Coding-Space <96582453+Stars-Universeal-Coding-Space@users.noreply.github.com>
2024-11-25 21:34:02 +1100

    Added Mushroom Stuff Grouping on the creative menu

--- src/main/java/net/sadmush/mushroomstuff/MushroomStuff.java
+++ src/main/java/net/sadmush/mushroomstuff/MushroomStuff.java
@@ -2,6 +2,7 @@ package net.sadmush.mushroomstuff;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.sadmush.mushroomstuff.item.ModItemGroups;
 import net.sadmush.mushroomstuff.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ public class MushroomStuff implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("MushroomStuff initialized | Hope you enjoy the funny mushroom blocks!");
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/sadmush/mushroomstuff/item/ModItemGroups.java
@@ -0,0 +1,22 @@
+package net.sadmush.mushroomstuff.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.sadmush.mushroomstuff.MushroomStuff;
+
+public class ModItemGroups {
+    public static final ItemGroup MUSHROOM_STUFF = Registry.register(Registries.ITEM_GROUP, new Identifier(MushroomStuff.MOD_ID, "spore"), FabricItemGroup.builder().displayName(Text.translatable("itemgroup.spore")).icon(() -> new ItemStack(Items.RED_MUSHROOM)).entries((displayContext, entries) -> {
+        entries.add(ModItems.SPORE);
+        entries.add(ModItems.GLOWING_SPORE);
+    }).build());
+    public static void registerItemGroups() {
+        MushroomStuff.LOGGER.info("Registering Item Groups " + MushroomStuff.MOD_ID);
+    }
+}
